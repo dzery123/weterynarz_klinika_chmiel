cd630bb8f29e8af94611113b2aaf93fb88c87e58
dzery123 <dzeryrep@gmail.com>
2025-12-18 08:14:22 +0100

    Poprawki w klasie Wizyta

--- src/weterynarska_klinika/app/Main.java
+++ src/weterynarska_klinika/app/Main.java
@@ -1,11 +1,24 @@
 package weterynarska_klinika.app;
 
 import weterynarska_klinika.enumy.GatunekZwierzecia;
+import weterynarska_klinika.model.KlinikaWetenaryjna;
 import weterynarska_klinika.model.Zwierze;
 
 public class Main {
     public static void main(String[] args) {
-        Zwierze fafik = new Zwierze("Fafik", GatunekZwierzecia.PIES);
+        KlinikaWetenaryjna klinika = new KlinikaWetenaryjna();
+        Zwierze z1 = new Zwierze("Fafik", GatunekZwierzecia.PIES);
+        Zwierze z2 = new Zwierze("Puszek", GatunekZwierzecia.KOT);
+        Zwierze z3 = new Zwierze("Artutek", GatunekZwierzecia.CHOMIK);
+        Zwierze z4 = new Zwierze("Ćwik Maksiu", GatunekZwierzecia.PAPUGA);
+
+
+
+
+
+
+
+
 
 
 
--- src/weterynarska_klinika/model/Wizyta.java
+++ src/weterynarska_klinika/model/Wizyta.java
@@ -6,15 +6,17 @@ public class Wizyta {
     private Zwierze zwierze;
     private LocalDate data;
     private String opis;
+    private double cena;
 
-    public Wizyta(LocalDate data, String opis, Zwierze zwierze) {
+    public Wizyta(LocalDate data, String opis, Zwierze zwierze, double cena) {
         this.data = data;
         this.opis = opis;
         this.zwierze = zwierze;
+        this.cena = cena;
     }
 
     @Override
     public String toString() {
-        return data + " - " + zwierze.getImie() + ": " + opis;
+        return data + " - " + zwierze.getImie() + ": " + opis + " usluga kosztowała: " + cena;
     }
 }
